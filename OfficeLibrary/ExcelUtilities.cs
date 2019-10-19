7a9403ca3a70da0816adf37382fa72c4b2196547
tunghoang <cayxungvenduong@gmail.com>
2019-10-19 17:53:30 +0700

    Add Function

--- OfficeLibrary/ExcelUtilities.cs
+++ OfficeLibrary/ExcelUtilities.cs
@@ -18,20 +18,23 @@ namespace OfficeLibrary
                 : path;
         }
 
-        public void ListToExcel<T>(List<T> list, bool isVisible = false,
-            bool isShowHeader = true, int startRow = 1, int startColumn = 1)
+     
+        public void ListToExcel<T>(List<T> list,bool isShowHeader=true, bool isVisible = false, int startRow = 1, int startColumn = 1)
         {
             // Init Export Excel app,workbook, worksheet
             var appExcel = new ApplicationClass {Visible = isVisible};
             var workbook = appExcel.Workbooks.Add(Missing.Value);
-            var sheet1 = workbook.Sheets[1];
+            var sheet1 = workbook.Sheets[1] as _Worksheet;
             var properties = list.First().GetType().GetProperties();
             var headers = properties.Select(o => o.Name).ToList();
             
             // Fill header
             if (isShowHeader)
             {
-                
+                for (var i = 0; i < headers.Count; i++)
+                {
+                    if (sheet1 != null) sheet1.Cells[startRow, startColumn + i] = headers[i];
+                }
             }
         }
     }
