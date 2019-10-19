22d2a6452594beb600a9250b707c5318b2f5c463
tunghoang <cayxungvenduong@gmail.com>
2019-10-19 17:32:50 +0700

    Init Library

---
+++ OfficeLibrary/ExcelUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
+
+namespace OfficeLibrary
+{
+    public class ExcelUtilities
+    {
+        public string ExportPath { get; }
+
+        public ExcelUtilities(string path = "")
+        {
+            ExportPath = path == string.Empty
+                ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{DateTime.Now:yyyyMMdd-HHmmss}.xlsx")
+                : path;
+        }
+
+        public void ListToExcel<T>(List<T> list, bool isVisible = false,
+            bool isShowHeader = true, int startRow = 1, int startColumn = 1)
+        {
+            // Init Export Excel app,workbook, worksheet
+            var appExcel = new ApplicationClass {Visible = isVisible};
+            var workbook = appExcel.Workbooks.Add(Missing.Value);
+            var sheet1 = workbook.Sheets[1];
+            var properties = list.First().GetType().GetProperties();
+            var headers = properties.Select(o => o.Name).ToList();
+            
+            // Fill header
+            if (isShowHeader)
+            {
+                
+            }
+        }
+    }
+}
