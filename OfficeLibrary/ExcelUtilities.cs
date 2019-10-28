145ac872189a94326ca4ba84aac6ea6ee4db9640
tunghoang <cayxungvenduong@gmail.com>
2019-10-28 14:05:00 +0700

    v1.0.0 release

--- OfficeLibrary/ExcelUtilities.cs
+++ OfficeLibrary/ExcelUtilities.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Office.Interop.Excel;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace OfficeLibrary
 {
@@ -20,70 +21,91 @@ namespace OfficeLibrary
         }
 
 
-        public void ListToExcel<T>(List<T> list, bool isShowHeader = true, bool isVisible = false, int startRow = 1,
-            int startColumn = 2, string indexName = "No")
+        public void ListToExcel<T>(IEnumerable<T> listExport, bool isShowHeader = true, bool isVisible = false,
+            uint startRow = 1,
+            uint startColumn = 1, string indexName = "No")
         {
-            if (startRow < 1 || startColumn < 2)
+            if (startRow < 1 || startColumn < 1)
                 throw new ArgumentException("Argument row start or column start is not valid.");
-            if (list == null || list.Count == 0) throw new ArgumentException("Argument list is null or not valid.");
-
+            if (listExport == null) throw new ArgumentException("Argument list is null or not valid.");
+            var list = listExport.ToList();
 
             // Init Export Excel app,workbook, worksheet
             var appExcel = new ApplicationClass {Visible = isVisible};
             var workbook = appExcel.Workbooks.Add(Missing.Value);
-            var sheet1 = workbook.Sheets[1] as _Worksheet;
-            var properties = list.First().GetType().GetProperties();
+            workbook.Activate();
+            var sheet1 = (_Worksheet) workbook.Sheets[1];
+            var properties = typeof(T).GetProperties();
             var headers = properties.Select(o => o.Name).ToList();
-            // Header Range
-            var rangeHeader = sheet1?.Range[sheet1.Cells[startRow, startColumn],
-                sheet1.Cells[startRow, startColumn + headers.Count - 1]];
 
-            // Fill header
-            var currentRow = 0;
-            if (isShowHeader)
-            {
-                for (var i = 0; i < headers.Count; i++)
+            if (sheet1 != null)
+            { 
+                // Header Range
+                var rangeHeader = sheet1.Range[sheet1.Cells[startRow, startColumn],
+                    sheet1.Cells[startRow, startColumn + headers.Count - 1]];
+
+                // Fill header
+                var currentRow = 0;
+                if (isShowHeader)
                 {
-                    if (sheet1 != null) sheet1.Cells[startRow, startColumn + i] = headers[i];
+                    for (var i = 0; i < headers.Count; i++)
+                    {
+                        sheet1.Cells[startRow, startColumn + i] = headers[i];
+                    }
+
+                    currentRow++;
                 }
 
-                currentRow++;
-            }
 
-            // Data Range            
-            var rangeData = sheet1?.Range[sheet1.Cells[startRow + currentRow, startColumn],
-                sheet1.Cells[startRow + currentRow + list.Count - 1, startColumn + headers.Count - 1]];
+                // Data Range            
+                var rangeData = sheet1.Range[sheet1.Cells[startRow + currentRow, startColumn],
+                    sheet1.Cells[startRow + currentRow + list.Count - 1, startColumn + headers.Count - 1]];
 
-            // Fill Data
-            foreach (var item in list)
-            {
-                for (var i = 0; i < headers.Count; i++)
+                // Fill Data
+                foreach (var item in list)
                 {
-                    if (sheet1?.Cells[currentRow + startRow, i + startColumn] is Range cell)
-                        cell.Value2 = GetPropValue(item, headers[i]);
+                    for (var i = 0; i < headers.Count; i++)
+                    {
+                        if (sheet1.Cells[currentRow + startRow, i + startColumn] is Range cell)
+                            cell.Value2 = GetPropValue(item, headers[i]);
+                    }
+
+                    currentRow++;
                 }
 
-                currentRow++;
+                Range rangeTotal;
+                if (isShowHeader && rangeHeader != null && rangeData != null)
+                {
+                    var freezeRow =(Range) sheet1.Rows[startRow+1];
+                    freezeRow.Select();
+                    
+                    workbook.Application.ActiveWindow.FreezePanes = true;
+                    var styleHeader = workbook.Styles.Add("MyExport Style");
+                    styleHeader.Font.Bold = true;
+
+                    rangeHeader.Style = styleHeader;
+                    rangeTotal = sheet1.Range[rangeHeader, rangeData];
+                }
+                else
+                {
+                    rangeTotal = rangeData;
+                }
+
+                if (rangeTotal != null)
+                {
+                    
+                    rangeTotal.Columns.AutoFit();
+                    rangeTotal.Borders.LineStyle = XlLineStyle.xlContinuous;
+                }
             }
 
-            Range rangeTotal;
-            if (isShowHeader && rangeHeader != null && rangeData != null)
-            {
-                var styleHeader = workbook.Styles.Add("MyExport Style");
-                styleHeader.Font.Bold = true;
-
-                rangeHeader.Style = styleHeader;
-                rangeTotal = sheet1.Range[rangeHeader, rangeData];
-            }
-            else
-            {
-                rangeTotal = rangeData;
-            }
-
-            if (rangeTotal != null) rangeTotal.Borders.LineStyle = XlLineStyle.xlContinuous;
             workbook.SaveAs(ExportPath, XlFileFormat.xlWorkbookDefault);
+            appExcel.Application.Workbooks.Close();
+            Marshal.FinalReleaseComObject(appExcel.Application.Workbooks);
             appExcel.Quit();
+            Marshal.FinalReleaseComObject(appExcel);
         }
+
 
         private static dynamic GetPropValue(object src, string propName)
         {
